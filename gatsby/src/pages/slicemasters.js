8066309bbfbfd2fece0b33fe8db3a11eebeb1473
Maxime GILBERT <mgilbert@weka.fr>
2020-10-26 12:51:39 +0100

    add navigation

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function SlicemastersPage() {
   return (
     <div>
+      <Nav />
       <p>Plop ! I'm the Slicemasters Page</p>
     </div>
   );
